e2ca4c927c9cc2a88af16d7d9645c1db447e3af7
guangzlu <guangzlu@amd.com>
2025-07-25 14:04:20 +0800

    fixed DecodeReqToTokenPool

--- python/sglang/srt/disaggregation/decode.py
+++ python/sglang/srt/disaggregation/decode.py
@@ -119,6 +119,20 @@ class DecodeReqToTokenPool:
     def clear(self):
         self.free_slots = list(range(self.size + self.pre_alloc_size))
 
+    def init_avaliable_token_for_single_req(self, indice, used_len, total_len):
+        self.available_token_id_for_reqs_start[indice] = used_len
+        self.available_token_id_for_reqs_end[indice] = total_len
+
+    def update_avaliable_token_start_for_single_req(self, indice):
+        self.available_token_id_for_reqs_start[indice] += 1
+
+    def check_if_single_req_have_avaliable_token(self, indice):
+        return self.available_token_id_for_reqs_start[indice] < self.available_token_id_for_reqs_end[indice]
+
+    def get_free_buffer_from_preallocated(self, indice):
+        return self.req_to_token[indice, self.available_token_id_for_reqs_start[indice]]
+
+
 
 @dataclass
 class DecodeRequest:
